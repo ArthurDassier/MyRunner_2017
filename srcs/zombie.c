3728b6b77574ed4642114aa01fc4c5ff94df1cb4
Arthur Dassier <arthur.dassier@epitech.eu>
2018-01-05 22:25:06 +0100

    Big array replaced by a while

--- srcs/zombie.c
+++ srcs/zombie.c
@@ -49,28 +49,12 @@ sfIntRect position_pixels(int a, int b, int c, int d)
 
 void init_tab(game_s *game)
 {
-	game->gh->animation[0] = position_pixels(500, 0, 300, 500);
-	game->gh->animation[1] = position_pixels(500, 500, 300, 500);
-	game->gh->animation[2] = position_pixels(500, 1000, 300, 500);
-	game->gh->animation[3] = position_pixels(500, 1500, 300, 500);
-	game->gh->animation[4] = position_pixels(500, 2000, 300, 500);
-	game->gh->animation[5] = position_pixels(500, 2500, 300, 500);
-	game->gh->animation[6] = position_pixels(500, 3000, 300, 500);
-	game->gh->animation[7] = position_pixels(500, 3500, 300, 500);
-	game->gh->animation[8] = position_pixels(500, 4000, 300, 500);
-	game->gh->animation[9] = position_pixels(500, 4500, 300, 500);
-	game->gh->animation[10] = position_pixels(500, 5000, 300, 500);
-	game->gh->animation[11] = position_pixels(500, 5500, 300, 500);
-	game->gh->animation[12] = position_pixels(500, 6000, 300, 500);
-	game->gh->animation[13] = position_pixels(500, 6500, 300, 500);
-	game->gh->animation[14] = position_pixels(500, 7000, 300, 500);
-	game->gh->animation[15] = position_pixels(500, 7500, 300, 500);
-	game->gh->animation[16] = position_pixels(500, 8000, 300, 500);
-	game->gh->animation[17] = position_pixels(500, 8500, 300, 500);
-	game->gh->animation[18] = position_pixels(500, 9000, 300, 500);
-	game->gh->animation[19] = position_pixels(500, 9500, 300, 500);
-	game->gh->animation[20] = position_pixels(500, 10000, 300, 500);
-	game->gh->animation[21] = position_pixels(500, 10500, 300, 500);
-	game->gh->animation[22] = position_pixels(500, 11000, 300, 500);
-	game->gh->animation[23] = position_pixels(500, 11500, 300, 500);
+	int	stand = 0;
+	int	i = 0;
+
+	while (i != 24) {
+		game->gh->animation[i] = position_pixels(500, stand, 300, 500);
+		stand = stand + 500;
+		++i;
+	}
 }
